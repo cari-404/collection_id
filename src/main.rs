86144af9e2e2aba89788700a0ee84fdbf3f234cc
cari-404 <135954797+cari-404@users.noreply.github.com>
2024-01-28 15:11:22 +0700

    tesr

--- src/main.rs
+++ src/main.rs
@@ -209,7 +209,6 @@ async fn some_function(start: &str, end: &str, v_code: &str, cookie_content: &st
 		progress_bar.set_style(ProgressStyle::default_bar()
 			.template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {percent}% {msg}")?
 			.progress_chars("█░"));
-		progress_bar
 	};
 
     for _ in 0..batch_count {
