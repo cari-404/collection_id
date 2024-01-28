f422438d09728dba58ef4da47cb30ee22c6305d7
cari-404 <135954797+cari-404@users.noreply.github.com>
2024-01-28 17:04:05 +0700

    out let pb: ProgressBar;

--- src/main.rs
+++ src/main.rs
@@ -192,24 +192,24 @@ async fn some_function(start: &str, end: &str, v_code: &str, cookie_content: &st
     let mut batch_number = 1;
     let mut current = start;
 
-	let pb: ProgressBar = if cfg!(windows) {
+	let pb: ProgressBar;
+	if cfg!(windows) {
 		#[cfg(windows)]
 		if OsVersion::current() <= OsVersion::new(6, 3, 0, 9800) {
-			let progress_bar = ProgressBar::hidden();
-			progress_bar
+			pb = ProgressBar::hidden();
 		} else {
 			let progress_bar = ProgressBar::new(batch_count.try_into().unwrap());
 			progress_bar.set_style(ProgressStyle::default_bar()
 				.template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {percent}% {msg}")?
 				.progress_chars("█░"));
-			progress_bar
+			pb = progress_bar;
 		}
 	} else {
-		let progress_bar: ProgressBar = ProgressBar::new(batch_count.try_into().unwrap());
+		let progress_bar = ProgressBar::new(batch_count.try_into().unwrap());
 		progress_bar.set_style(ProgressStyle::default_bar()
 			.template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {percent}% {msg}")?
 			.progress_chars("█░"));
-		progress_bar
+		pb = progress_bar;
 	};
 
     for _ in 0..batch_count {
